c7cdcd09f03f7616dd71dc30294c438f46b9f575
ckroeger <ckroeger.hh@gmail.com>
2024-05-20 14:16:18 +0200

    base tailwind layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleToogleLayer() {
+    const layer = document.getElementById('layer')
+    layer?.classList.toggle('hidden')
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="bg-zinc-100 min-h-screen flex flex-col justify-center items-center relative">
+        <header className="fixed top-0 w-full text-lg text-left px-4 py-2 bg-white shadow-md z-10">
+          <button
+            id="toggleLayer"
+            className="focus:outline-none"
+            onClick={handleToogleLayer}>
+            🕵️‍♂️ NMC - Newman-Manager-Client
+          </button>
+        </header>
+        <div
+          id="layer"
+          className="hidden top-full w-full bg-white shadow-md z-100">
+          <div className="p-4">
+            <div className="rounded-lg bg-zinc-200 p-2 mb-2">Entry 1</div>
+            <div className="rounded-lg bg-zinc-200 p-2 mb-2">Entry 2</div>
+            <div className="rounded-lg bg-zinc-200 p-2 mb-2">Entry 3</div>
+          </div>
+        </div>
+        <button
+          onClick={handleToogleLayer}
+          className="text-white px-4 py-2 rounded-lg shadow-md">
+          Click me
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
